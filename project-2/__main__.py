bebb93fb3f19001922153d9c8f23d6b02bfebdeb
literallypossessed <st8565167@gmail.com>
2026-02-14 21:07:04 -0500

    Simple user access script

--- project-2/__main__.py
+++ project-2/__main__.py
@@ -1,15 +1,9 @@
-name = 'Alice'
-print(name, type(name))
+correct_name = 'Jerry'
+name =(input('What is your name:'))
 
-is_student = True
-print(is_student, type(is_student))
-
-age = 20
-print(age, type(age))
-
-score = 80.5
-print(isinstance(score, float))
-print(score, type(score))
-
+if name == correct_name:
+    print('Access Successful')
+else:
+    print('Access Denied')
 
 
